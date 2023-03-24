efa10e898f6208beea4e7627480d39c4fa50e90a
Benjie Gil Angeles <126475746+thebenjieangeles@users.noreply.github.com>
2023-03-25 01:21:38 +0700

    Add files via upload

---
+++ Fizz-buzz/script.js
@@ -0,0 +1,15 @@
+let input = Number(
+  prompt("Please enter the number you would like to FizzBuzz")
+);
+
+for (let i = 1; i <= input; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
